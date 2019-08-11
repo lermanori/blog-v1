90212c84e78bfa974bbb0ed98a20787f62b5efaa
lermanori <lermanori@gmail.com>
2019-08-11 15:39:27 +0300

    'commit'

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,8 @@ module.exports = {
 
 
 
-    ]
+    ],
+
   },
   /*
    ** Customize the progress-bar color
@@ -56,7 +57,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/hotjar'],
   /*
    ** Nuxt.js modules
    */
